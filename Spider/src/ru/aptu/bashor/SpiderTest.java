7356a2761b0fc717d097d729c079be4da6d44ee1
zalim <zalim@zlaptop.(none)>
2011-11-21 08:26:31 +0400

    add JUnit and empty SpiderTest

---
+++ Spider/src/ru/aptu/bashor/SpiderTest.java
@@ -0,0 +1,17 @@
+package ru.aptu.bashor;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: zalim
+ * Date: 11/21/11
+ * Time: 7:16 AM
+ */
+public class SpiderTest extends TestCase {
+    @Test
+    public void testRun() throws Exception {
+        assertTrue(false);
+    }
+}
